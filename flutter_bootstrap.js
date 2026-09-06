742c0d5579535d423777eafd4ae08513c86c3ded
eramuse74-hub <eramuse74-hub@users.noreply.github.com>
2026-09-06 14:51:29 +0000

    Deploy 126c8e66686ea6e8d775b6f775411a78ed1d2041 eramuse74-hub/Ayomath@126c8e66686ea6e8d775b6f775411a78ed1d2041

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3674846881" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "193834572" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
